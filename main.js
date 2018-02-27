b092f8711c2a52acbe5e0cc09d72a145bb3da4d9
Marcin Szczepanski <marcin.l.szczepanski@gmail.com>
2018-02-27 21:39:20 +0900

    Add method for creating genesis block

--- main.js
+++ main.js
@@ -13,3 +13,13 @@ class Block {
     return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
   }
 }
+
+class Blockchain {
+  constructor() {
+    this.chain = [];    // array of blocks
+  }
+  
+  createGenesisBlock() {
+    return new Block(0, "01/01/2018", "Genesis block", "0");
+  }
+}
