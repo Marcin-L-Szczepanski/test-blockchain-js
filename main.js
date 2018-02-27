a9369a1f9c4107a367de18a889b1ac8397c4485f
Marcin Szczepanski <marcin.l.szczepanski@gmail.com>
2018-02-27 21:42:34 +0900

    Add method for adding a new block

--- main.js
+++ main.js
@@ -23,4 +23,13 @@ class Blockchain {
     return new Block(0, "01/01/2018", "Genesis block", "0");
   }
   
+  getLatestBlock() {
+    return this.chain(this.chain.length - 1);
+  }
+  
+  addBlock(newBlock) {
+    newBlock.previousHash = this.getLatestBlock().hash;
+    newBlock.hash = newBlock.calculateHash();
+    this.chain.push(newBlock);
+  }
 }
